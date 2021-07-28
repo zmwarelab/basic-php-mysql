e96bf737dfe59364224df6fd13476877c41ab569
Intern 04 <77699456+zm-intern04@users.noreply.github.com>
2021-07-28 11:45:25 -0400

    Connection String

--- inc/connection.inc.php
+++ inc/connection.inc.php
@@ -4,4 +4,12 @@ $dns = 'mysql:host=localhost;dbname=demodb';
 $username = 'root';
 $password = '';
 
-$db = new PDO($dns, $username, $password);
+//$db = new PDO($dns, $username, $password);
+$db = mysqli_connect($dns, $username, $password);
+
+if(!$db){
+  echo "Connection Failed! 🤣";
+}
+echo "Connected Successfull";
+
+?>
